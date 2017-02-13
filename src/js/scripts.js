afdb7b4f2b55e10d69fbf166d9b65d979801d915
Eric Closquet <closquet.eric@live.be>
2017-02-13 10:53:36 +0100

    Exercice part two

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,6 +7,19 @@
 *
 */
 
+const fHandleTabClick = function (oEvent) {
+    oEvent.preventDefault();
+    console.log("yop");
+    const $target = oEvent.currentTarget;
+    if ($target.parentNode.classList.contains( "active" )) {
+        return;
+    }
+    document.querySelector( "ul.nav-tabs li.active" ).classList.remove( "active" );
+    $target.parentNode.classList.add( "active" );
+    document.querySelector( ".tab-content .tab-pane.active" ).classList.remove( "active" );
+    document.getElementById( $target.getAttribute( "data-tab-target" ) ).classList.add( "active" );
+};
+
 window.addEventListener( "load", function () {
     // 1. select a with rel=external attributes
     Array.from(document.querySelectorAll( 'a[rel*="external"]')).forEach( function ( $elt ) {
@@ -17,4 +30,8 @@ window.addEventListener( "load", function () {
         $elt.setAttribute( "target", "_new" );
     } );
 
+    // 2. tab mecanism
+    Array.from(document.querySelectorAll( 'ul.nav.nav-tabs a')).forEach( function ( $elt ) {
+        $elt.addEventListener( "click", fHandleTabClick );
+    })
 });
