048bc6d4c8195c4a8d2450c83dd6b0ef38471b9a
Eric Closquet <closquet.eric@live.be>
2017-02-13 13:08:04 +0100

    add  part1 and 2

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+/* eric/meet-jquery
+*
+* /gulpfile.js - gulp tasks
+*
+* code by email...
+* started at 13/02/2017
+*
+*/
+
+
+
+const fChangeTab = function () {
+    oEvent.preventDefault();
+    $tabs.parent().filter( ".active").removeClass( "active" );
+    $( this ).parent().addClass( "active" );
+    $( ".tab-content .tab-pane.active" ).removeClass( " ctive" );
+    $( `#${ $( this ).data( "tab-target" )}` )..addClass( "active" );
+}
+
+//called when dom is loaded
+$( function () {
+
+    // 1. link with rel=external
+    $( 'a[rel="external"]').attr( "target ", "_new" );
+
+    // 2. tab
+    $tabs = $( 'ul.nav.nav-tabs a').on( "click ", fChangeTab );
+
+
+
+    //part1
+
+
+
+
+
+
+
+
+})
